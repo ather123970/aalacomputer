32eb15e98f6044d5acd71742d8696cb3c29a8333
Ather24-pixels <businessman2124377@gmail.com>
2025-11-07 04:07:31 -0800

    Emptying old repo

--- Music/aalacomputer-master/api/ping.js
+++
@@ -1,7 +0,0 @@
-import { withCors } from './_lib/cors.js';
-
-export default function handler(req, res) {
-  withCors(req, res);
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  res.status(200).json({ ok: true, ts: Date.now() });
-}
